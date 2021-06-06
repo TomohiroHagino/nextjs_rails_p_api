ef86127af8380a23901ecf602a8ef98a5377d032
Hagino T <marspeoplehg@gmail.com>
2021-06-07 00:23:04 +0900

    Devise token auth 実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  namespace :api do
+    scope :v1 do
+      mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+          registrations: 'api/v1/auth/registrations'
+      }
+    end
+  end
   resources :posts
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
