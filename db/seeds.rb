9fff2abb3a0ff09ac218029f8c882914cc6a99a8
Hagino T <marspeoplehg@gmail.com>
2021-06-06 22:17:02 +0900

    first commit

---
+++ db/seeds.rb
@@ -0,0 +1,19 @@
+Post.create!(
+  [
+    {
+      title: 'Next.js + Ruby on Rails + Docker の環境構築'
+    },
+    {
+      title: 'React Hooks でカスタムフックを作る'
+    },
+    {
+      title: 'GraphQL と Apollo Client 入門'
+    },
+    {
+      title: '【TypeScript4.3】Template Literal Types'
+    },
+    {
+      title: 'Tailwind CSS でダークモード実装'
+    },
+  ]
+)
