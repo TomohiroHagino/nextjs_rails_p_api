8d5ce70a511947abd9ca44c4fb004bab4773dd66
Hagi T <marspeoplehg@gmail.com>
2022-04-23 14:22:18 +0400

    ログに書き込まれるように

--- app/controllers/api/validates_controller.rb
+++ app/controllers/api/validates_controller.rb
@@ -8,7 +8,10 @@ class Api::ValidatesController < ApplicationController
   def validate_check(validate_obj)
     MyselfSkillValidator.new(validate_obj).validate
     { status: "ok" }
-  rescue MyselfSkillValidator::Error => e
+  rescue MyselfSkillValidator::MyselfSkillValidatorError => e
+    logger.warn "---------------レコードの保存に失敗しました。" 
+    logger.warn e
+    logger.warn "---------------" 
     { status: "ng", messages: e.message }
   end
 end
