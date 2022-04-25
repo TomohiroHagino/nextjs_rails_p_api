320fd27e0607c389d8e80e9d0c8fa2662bc2177b
Hagi T <marspeoplehg@gmail.com>
2022-04-25 14:51:22 +0400

    クエリの修正

--- app/queries/mes/myself_react_and_next_js_skills_query.rb
+++ app/queries/mes/myself_react_and_next_js_skills_query.rb
@@ -5,7 +5,10 @@ module Mes
     end
 
     def call
-      @relation.eager_load(:react_skills,:next_js_skills)
+      @relation.select('
+        react_skills.id,react_skills.title,react_skills.body,
+        next_js_skills.id,next_js_skills.title,next_js_skills.body')
+               .joins(:react_skills,:next_js_skills)
                .where(react_skills: { deleted: false })
                .where(next_js_skills: { deleted: false })
                .find_by(mes: {id: Rails.application.credentials.me})
